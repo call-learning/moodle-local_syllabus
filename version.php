b9ebea2e6adaa2b4f022a92b95166a820a63383e
Laurent David <lmedavid@gmail.com>
2023-08-17 14:57:50 +0200

    Fix issue with date and content with title
    * If we display a date with title in the content of the page
    the title is "h2" and too big for the label

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_syllabus';
-$plugin->release = '0.2.1';
-$plugin->version = 2023081200;
+$plugin->release = '0.2.2';
+$plugin->version = 2023081201;
 $plugin->requires = 2019111800;
 $plugin->maturity = MATURITY_ALPHA;
